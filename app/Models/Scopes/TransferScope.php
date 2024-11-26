ba2464c48744a816d3daac78ee1cf0184cf9f024
Alexandre Bagio <alexandre.bagio3@gmail.com>
2024-11-26 16:13:41 -0300

    fix - visualize payee list

--- app/Models/Scopes/TransferScope.php
+++ app/Models/Scopes/TransferScope.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Scopes;
 
+use App\Enums\TransferSituation;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
@@ -10,9 +11,14 @@ class TransferScope implements Scope
 {
     public function apply(Builder $builder, Model $model): void
     {
+        $userId = auth()->user()->id;
+
         $builder->where(
-            fn ($q) => $q->where('payer', auth()->user()->id)
-                ->orWhere('payee', auth()->user()->id)
+            fn ($q) => $q->where('payer', $userId)
+                ->orWhere(
+                    fn ($qs) => $qs->where('payee', $userId)
+                        ->where('situation', TransferSituation::Finish->value)
+                )
         );
     }
 }
